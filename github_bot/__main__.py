96b1fa2c72941255d53161d85f7487a49960d06b
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-03-24 14:01:49 +0100

    Initial bot

---
+++ github_bot/__main__.py
@@ -0,0 +1,9 @@
+from octomachinery.app.server.runner import run as run_app
+
+
+if __name__ == "__main__":
+    run_app(
+        name='PyCon-Bot-by-webknjaz',
+        version='1.0.0',
+        url='https://github.com/apps/pyyyyyycoooon-booooot111',
+    )
